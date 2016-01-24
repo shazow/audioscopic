83aa89798ec8e4bcd66df5d25a834be5af3ec2ce
Andrey Petrov <andrey.petrov@shazow.net>
2016-01-24 13:10:37 -0800

    main skeleton.

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/alexcesaro/log"
+	"github.com/alexcesaro/log/golog"
+	"github.com/jessevdk/go-flags"
+)
+
+// version gets replaced during build
+var version string = "dev"
+
+// logger gets replaced by golog
+var logger log.Logger = log.NullLogger
+
+// Options contains the flag options
+type Options struct {
+	Verbose []bool `short:"v" long:"verbose" description:"Show verbose logging."`
+	Version bool   `long:"version"`
+}
+
+var logLevels = []log.Level{
+	log.Warning,
+	log.Info,
+	log.Debug,
+}
+
+func fail(code int, format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, args...)
+	os.Exit(code)
+}
+
+func main() {
+	options := Options{}
+	parser := flags.NewParser(&options, flags.Default)
+	_, err := parser.Parse()
+	if err != nil {
+		os.Exit(1)
+		return
+	}
+
+	if options.Version {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
+	// Figure out the log level
+	numVerbose := len(options.Verbose)
+	if numVerbose > len(logLevels) {
+		numVerbose = len(logLevels) - 1
+	}
+
+	logLevel := logLevels[numVerbose]
+	logger = golog.New(os.Stderr, logLevel)
+
+	// TODO: Work here.
+
+	logger.Info("Done.")
+	os.Exit(0)
+}
